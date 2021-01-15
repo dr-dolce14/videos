9014b7082860291023e31779d682fbe729aca2f1
dr-dolce14 <dolcemascop1@gmail.com>
2021-01-15 16:43:47 -0500

    video detail

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import SearchBar from './SearchBar'
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 
 
 class App extends React.Component {
 
-  state = { videos: [], selectVideo: null };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
@@ -19,7 +20,7 @@ class App extends React.Component {
   }
 
   onVideoSelect = (video) => {
-    console.log('from the app!', video)
+    this.setState({ selectedVideo: video })
   } 
 
 
@@ -27,6 +28,7 @@ class App extends React.Component {
   return (
     <div className="ui container">
       <SearchBar onFormSubmit={this.onTermSubmit} />
+      <VideoDetail video={this.state.selectedVideo}/>
       <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/>
     </div>
   );
